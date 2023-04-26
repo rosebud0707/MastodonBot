64a0599619e1e316ba9fb57216dc8b91892a86d9
rosebud0707 <110314722+rosebud0707@users.noreply.github.com>
2023-04-27 00:48:13 +0900

    Add files via upload

--- MastodonService.py
+++ MastodonService.py
@@ -3,6 +3,7 @@
 """
 from mastodon import Mastodon, StreamListener
 import GenerateToot
+import hashlib
 
 class MastodonService:
 
@@ -36,24 +37,37 @@ class Stream(StreamListener):
         """
         try:
             if notif['type'] == 'mention':
-                content = notif['status']['content']
-                content = content.rsplit(">")[-2].split("<")[0].strip() #タグ除去
-                
-                if content == str(self.initValues.stop):
-                    self.mastodon.toot('強制終了します。')
-                    exit()
-                else:
-                    id = notif['status']['account']['username']
-                    st = notif['status']
-                    self.logger.info("@" + str(id) + "さんへ返信処理開始")
-                    
-                    content = str(content) + " 必ず500文字以内で簡潔に回答してください。"
+                content_raw = notif['status']['content']
+                content = content_raw.rsplit(">")[-2].split("<")[0].strip() # リプライよりhtmlタグ除去。リプライ文だけを取得。
+                st = notif['status'] # ステータス情報全般 JSON形式
+                id = notif['status']['account']['username'] # id
+                uri = notif['status']['uri']
 
+                if 'emergency' in content:
+                    if hashlib.sha256(str(content).encode('ascii')).hexdigest() == str(self.initValues.stop):
+                        self.mastodon.status_post('強制終了します。', visibility = 'unlisted')
+                        exit()
+
+                elif self.initValues.mastodon_bot_api_base_url not in str(uri):
+                    self.mastodon.status_reply(st, '特定のインスタンスのアカウント以外には返答できません。', id, visibility = 'unlisted')
+
+                else:
+                    st = notif['status'] # ステータス情報全般 JSON形式
+                    id = notif['status']['account']['username'] # id
+                    disp_name = notif['status']['account']['display_name'] # 表示名
+                    
+                    if notif['status']['visibility'] == 'direct': # 公開範囲設定。directでリプライされた際はdirectで、それ以外はunlistedで返答を行う。
+                        visibility = self.initValues.mastodon_bot_visibility_direct
+                    else:
+                        visibility = self.initValues.mastodon_bot_visibility_unlisted
+                    
+                    self.logger.info("@" + str(id) + "さんへ返信処理開始")
+                    content = str(content) + "必ず500文字以内で簡潔に回答してください。"
                     self.logger.info("質問文:" + str(content))
 
-                    res = self.generateToots.gen_msg(content)
+                    res = self.generateToots.gen_msg(content) # 回答文生成
 
-                    self.mstdn_procedure.do_toot(res, id, st)
+                    self.mstdn_procedure.do_toot(res, id, st, visibility) # トゥート
 
         except Exception as e:
             self.logger.critical("通知の受信に関して、エラーが発生しました。" + str(e))
@@ -68,7 +82,7 @@ class MastodonProcedure:
         self.mastodon = mastodon
         self.initValues = initValues
 
-    def do_toot(self, response, id, st):
+    def do_toot(self, response, id, st, visibility_param):
         """トゥート処理
             生成文書を編集し、トゥートする。
             Args:
@@ -79,6 +93,10 @@ class MastodonProcedure:
         try:
             response = str(response).replace('@', '＠') # 対象者以外へのリプライ防止
 
+            if response == 'None':
+                self.mastodon.status_post('予期せぬエラーの発生。強制終了します。', visibility = 'unlisted') # bot同士の会話、予期せぬエラーでの暫定対応策
+                exit()
+
             if len(response) > 480:  # トゥート上限エラー回避。バッファをみて480字超過時は分割。
                 length = len(response)
                 splitLine =  [response[i:i+300] for i in range(0, length, 300)]
@@ -88,12 +106,12 @@ class MastodonProcedure:
                     self.mastodon.status_reply(st,
                                 str(splitLine[num]),
                                 id,
-                                visibility = self.initValues.mastodon_bot_visibility_unlisted)
+                                visibility = visibility_param)
             else:
                 self.mastodon.status_reply(st,
                         str(response),
                         id,
-                        visibility = self.initValues.mastodon_bot_visibility_unlisted)
+                        visibility = visibility_param)
                 
         except Exception as e:
             self.logger.critical("トゥート処理にて、エラーが発生しました。" + str(e))
