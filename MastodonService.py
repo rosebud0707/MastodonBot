6e656443f6dbdf3ba6cad581c58bdb0519e0ffaf
rosebud0707 <110314722+rosebud0707@users.noreply.github.com>
2023-05-03 02:27:25 +0900

    バグ修正
    リプライ誘導の回避。(返信をトゥートする前に、本文中に「@」が含まれていれば全角化し、リプライとなることを回避。)
    URLが含まれるリプライを受けた際に未入力扱いとなる現象の修正。またURL自体も認識するように修正。

--- MastodonService.py
+++ MastodonService.py
@@ -2,6 +2,7 @@
     Mastodonに関連する処理
 """
 from mastodon import Mastodon, StreamListener
+from bs4 import BeautifulSoup 
 import GenerateToot
 import hashlib
 
@@ -40,11 +41,12 @@ class Stream(StreamListener):
             if notif['type'] == 'mention':
                 self.logger.info("mentionの検知")
 
-                content_raw = notif['status']['content']
-                content = content_raw.rsplit(">")[-2].split("<")[0].strip() # リプライよりhtmlタグ除去。リプライ文だけを取得。
+                # 各種項目の取得、設定。
                 st = notif['status'] # ステータス情報全般 JSON形式
                 id = notif['status']['account']['username'] # id
                 uri = notif['status']['uri'] # ユーザのインスタンスURI
+                content_raw = notif['status']['content'] # リプライ内容
+                content = self.EditContents(content_raw) # 質問文の編集
 
                 if notif['status']['visibility'] == 'direct': # 公開範囲設定。directでリプライされた際はdirectで、それ以外はunlistedで返答を行う。
                     visibility_status = self.initValues.mastodon_bot_visibility_direct
@@ -54,11 +56,11 @@ class Stream(StreamListener):
                 if self.initValues.mastodon_bot_api_base_url not in str(uri): # インスタンスチェック
                     self.logger.warning("別インスタンスからのリプライ")
 
-                elif len(notif['status']['mentions']) > 1: # 他アカウントへのリプライ防止
+                elif len(notif['status']['mentions']) > 1: # 質問者以外のアカウントへのリプライ防止
                     self.logger.warning("複数アカウント検知")
 
-                elif 'emergencystopsignal' in str(content):
-                    if hashlib.sha256(str(content).encode('ascii')).hexdigest() == str(self.initValues.stop): # 緊急停止信号発報確認
+                elif 'emergencystopsignal' in str(content): # 緊急停止信号発報確認
+                    if hashlib.sha256(str(content).encode('ascii')).hexdigest() == str(self.initValues.stop):
                         self.logger.warning("緊急停止信号発報 プログラムの終了")
                         self.mastodon.status_post('緊急停止信号検知。強制終了します。', visibility = 'unlisted')
                         exit()
@@ -67,9 +69,10 @@ class Stream(StreamListener):
                     self.logger.warning("質問未入力")
                     self.mastodon.status_reply(st, '質問内容を入力してください。', id, visibility = visibility_status)
 
-                else:                                        
+                else:
+                    # 正常処理                                        
                     self.logger.info('@' + str(id) + "さんへ返信処理開始")
-                    content = str(content) + "必ず500文字以内で簡潔に回答してください。"
+                    content = "質問します。" + content
                     self.logger.info("質問文:" + str(content))
 
                     res = self.generateToots.gen_msg(content) # 回答文生成
@@ -78,7 +81,36 @@ class Stream(StreamListener):
 
         except Exception as e:
             self.logger.critical("通知の受信に関して、エラーが発生しました。" + str(e))
+    
+    def EditContents(self, content_raw):
+        '''質問内容編集
+            取り出したリプライの情報より、質問文を編集する。
+        '''
+        try:
+            self.logger.info("質問文の編集処理開始")
 
+            html_data = BeautifulSoup(content_raw, "html.parser")
+
+            links = html_data.find_all("a") # リンクを抽出
+
+            # URLを連結する
+            content_links = ""
+            if len(links) > 1:
+                for i in range(1, len(links)):
+                    content_links += links[i].get("href") + ' '
+
+            # リプライ本文を抜き出す。
+            if html_data.find("span").next_sibling == None:
+                content = ""
+            else:
+                content = html_data.find("span").next_sibling
+            
+            # リプライ本文とURLを結合する。
+            return content + content_links
+        
+        except Exception as e:
+            self.logger.critical("質問文編集処理で、エラーが発生しました。" + str(e))        
+        
 class MastodonProcedure:
     """Mastodonの各処理を行う
     """
@@ -102,6 +134,8 @@ class MastodonProcedure:
                 self.logger.critical("予期せぬエラーの発生。")
                 self.mastodon.status_post('予期せぬエラーの発生。強制終了します。', visibility = 'unlisted') # bot同士の会話、予期せぬエラーでの暫定対応策
                 exit()
+
+            response = str(response).replace('@', '＠') # リプライの防止
 
             self.logger.info("トゥート")
             if len(response) > 480:  # トゥート上限エラー回避。バッファをみて480字超過時は分割。
